995f1bcac9622ae80314829964502e484ebe2af7
Rishiande <96639486+Rishiande@users.noreply.github.com>
2022-08-30 15:59:14 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,2 +1,2 @@
 import streamlit as st
-st.title('Santhosh dimple best friends')
+st.title('Rishi hheart lo shiva sai sowji sushma ramya rupa mouna bhargavi varshini aditya bujji loves and pavani pavani pavani pavani na mansulo undipoyi pathukupoyaru')
